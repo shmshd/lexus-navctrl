897e4d43fd7f6ba301def71855dd19fc93e27774
Shamshid <shmshd12@gmail.com>
2026-03-19 14:55:48 +0400

    add custom satnav keyboard config

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,126 @@
 #include <SoftwareSerial.h>
 #include <LexusNavController.h>
+#include <HID-Project.h>
 
 #define DATA_PIN 8 // D8
 
+// RX = D8 (remote line), TX = NULL (unused)
 SoftwareSerial navUart(DATA_PIN, NULL);
 LexusNavController nav(navUart);
 
-static void printHexFrame(const uint8_t *f, uint8_t n)
-{
-    for (uint8_t i = 0; i < n; ++i)
-    {
-        if (f[i] < 0x10)
-            Serial.print('0');
-        Serial.print(f[i], HEX);
-        if (i + 1 < n)
-            Serial.print(' ');
-    }
+static void printHexFrame(const uint8_t* f, uint8_t n) {
+  for (uint8_t i = 0; i < n; ++i) {
+    if (f[i] < 0x10) Serial.print('0');
+    Serial.print(f[i], HEX);
+    if (i + 1 < n) Serial.print(' ');
+  }
 }
 
-static void blinkLED()
-{
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(300);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(300);
+void setup() {
+  Serial.begin(115200);
+  navUart.begin(2400);
+  // We handle printing ourselves (no auto-print) and disable global HEX debug
+  nav.begin(/*showHex=*/false, /*autoPrint=*/false);
+  nav.setFrameTimeoutMs(40);
+  nav.setDebounceMs(180);
+  Consumer.begin();
+  Keyboard.begin();
+  System.begin();
 }
 
-void setup()
-{
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(115200);
-    navUart.begin(2400);
-    nav.begin(/*showHex=*/false, /*autoPrint=*/false);
-    nav.setFrameTimeoutMs(40);
-    nav.setDebounceMs(180);
-    Serial.println(F("Ready for signals!"));
-}
+void loop() {
+  nav.update();
 
-void loop()
-{
-    nav.update();
-    switch (nav.available() && nav.press)
-    {
-        // Main buttons
-        case LNR_BTN_OFF: Serial.println(F("OFF")); break;
-        case LNR_BTN_DISP: Serial.println(F("DISP")); break;
-        case LNR_BTN_INFO: Serial.println(F("INFO")); break;
-        case LNR_BTN_BACK: Serial.println(F("BACK")); break;
-        case LNR_BTN_MENU: Serial.println(F("MENU")); break;
-        case LNR_BTN_DEST: Serial.println(F("DEST")); break;
-        case LNR_BTN_MAP: Serial.println(F("MAP")); break;
-        case LNR_BTN_MINUS: Serial.println(F("MINUS")); break;
-        case LNR_BTN_PLUS: Serial.println(F("PLUS")); break;
-        case LNR_BTN_UP: Serial.println(F("UP")); break;
-        case LNR_BTN_DOWN: Serial.println(F("DOWN")); break;
-        case LNR_BTN_RIGHT: Serial.println(F("RIGHT")); break;
-        case LNR_BTN_LEFT: Serial.println(F("LEFT")); break;
-        case LNR_BTN_ENTER: Serial.println(F("ENTER")); break;
-
-        // Joystick
-        case LNR_BTN_UP_RIGHT: Serial.println(F("UP-RIGHT")); break;
-        case LNR_BTN_UP_LEFT: Serial.println(F("UP-LEFT")); break;
-        case LNR_BTN_DOWN_RIGHT: Serial.println(F("DOWN-RIGHT")); break;
-        case LNR_BTN_DOWN_LEFT: Serial.println(F("DOWN-LEFT")); break;
-
-        default:
-        {
-            // Unknown code: show raw HEX to capture new patterns
-            Serial.print(F("UNKNOWN  "));
-            printHexFrame(nav.lastFrame(), nav.lastFrameLen());
-            Serial.println();
-        }
+  if (nav.available()) {
+    switch (nav.press) {
+      // Main buttons
+      case LNR_BTN_OFF:
+          // Keyboard.press(KEY_LEFT_GUI);
+          // Keyboard.press(KEY_L);
+          // Keyboard.releaseAll();
+        System.write(HID_SYSTEM_SETUP);
+        Serial.println(F("OFF")); 
         break;
-    }
+      case LNR_BTN_DISP:
+        System.write(HID_SYSTEM_SETUP);
+        Serial.println(F("DISP"));   
+        break;
+      case LNR_BTN_INFO:
+        System.write(HID_SYSTEM_SPEAKER_MUTE);
+        Serial.println(F("INFO"));   
+        break;
+      case LNR_BTN_BACK:
+          Keyboard.write(HID_KEYBOARD_ESCAPE);
+          Serial.println(F("BACK"));   
+        break;
+      case LNR_BTN_MENU:
+          Serial.println(F("MENU"));   
+        break;
+      case LNR_BTN_DEST:
+          Serial.println(F("DEST"));   
+        break;
+      case LNR_BTN_MAP:
+          Consumer.write(MEDIA_PLAY_PAUSE);
+          Serial.println(F("MAP"));    
+        break;
+      case LNR_BTN_MINUS:
+          Consumer.write(MEDIA_VOLUME_DOWN);  
+          Serial.println(F("MINUS"));  
+        break;
+      case LNR_BTN_PLUS: 
+          Consumer.write(MEDIA_VOLUME_UP);  
+          Serial.println(F("PLUS"));   
+        break;
+      case LNR_BTN_ENTER:
+          Keyboard.write(KEY_ENTER);
+          Serial.println(F("ENTER"));  
+        break;
 
-    if (nav.available() && nav.press) blinkLED();
+      // Joystick
+      case LNR_BTN_UP:
+        Keyboard.write(KEY_UP_ARROW);
+        Serial.println(F("UP"));
+        break;
+      case LNR_BTN_DOWN:
+        Keyboard.write(KEY_DOWN_ARROW);
+        Serial.println(F("DOWN"));   
+        break;
+      case LNR_BTN_RIGHT:
+        Keyboard.write(KEY_RIGHT_ARROW);
+        Serial.println(F("RIGHT"));  
+        break;
+      case LNR_BTN_LEFT:
+        Keyboard.write(KEY_LEFT_ARROW);
+          Serial.println(F("LEFT"));   
+        break;
+      case LNR_BTN_UP_RIGHT:
+          Keyboard.write(KEY_UP_ARROW);
+          Keyboard.write(KEY_RIGHT_ARROW);
+          Serial.println(F("UP-RIGHT"));   
+        break;
+      case LNR_BTN_UP_LEFT:
+          Keyboard.write(KEY_UP_ARROW);
+          Keyboard.write(KEY_LEFT_ARROW);
+          Serial.println(F("UP-LEFT"));    
+        break;
+      case LNR_BTN_DOWN_RIGHT:
+          Keyboard.write(KEY_DOWN_ARROW);
+          Keyboard.write(KEY_RIGHT_ARROW);
+          Serial.println(F("DOWN-RIGHT")); 
+        break;
+      case LNR_BTN_DOWN_LEFT:
+          Keyboard.write(KEY_DOWN_ARROW);
+          Keyboard.write(KEY_LEFT_ARROW);
+          Serial.println(F("DOWN-LEFT"));  
+       break;
+
+      default: {
+        // Unknown code: show raw HEX so you can capture new patterns
+        Serial.print(F("UNKNOWN  "));
+        printHexFrame(nav.lastFrame(), nav.lastFrameLen());
+        Serial.println();
+      } break;
+    }
+  }
 }
+
